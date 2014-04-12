91be1e24ac826529d7903a88916b562e83f6f6cb
Richard Walker <digitalsadhu@gmail.com>
2014-04-12 18:50:11 +0200

    Add files needed for tests and testing

---
+++ test/envoodoo.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var envoodoo  = require('../index.js')
+  , expect    = require('chai').expect
+
+describe('envoodoo module', function () {
+
+  describe('loads a given env file', function () {
+
+    it('loads a .env file if no path provided', function () {
+      expect(process.env._DB_HOST).to.be.an('undefined')
+      envoodoo()
+      expect(process.env._DB_HOST).to.equal('localhost')
+    })
+
+    it('loads a .env-test file', function () {
+      expect(process.env.DB_HOST).to.be.an('undefined')
+      envoodoo(__dirname + '/.env-test')
+      expect(process.env.DB_HOST).to.equal('localhost')
+    })
+
+    it('returns false if file was not loaded', function () {
+      expect(envoodoo('some/fake/path')).to.equal(false)
+    })
+
+  })
+
+})
